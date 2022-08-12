499ccea326c591b7936d92489cb01e37ff4902b8
Javi <79421409+javinoto@users.noreply.github.com>
2022-08-11 20:40:23 -0400

    Correción

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title('TUTURIAL DE PYTHON CON STREAMLIB')
 st.text('Hello MOJIX')
 
-//st.header('This is a header')
-//st.subheader('This is a subheader')
+# st.header('This is a header')
+# st.subheader('This is a subheader')
 
 st.markdown('Vamos a ver lo básico pero poderoso de Python')
 
