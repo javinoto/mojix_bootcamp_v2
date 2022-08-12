8d6ada4d5377c3e1ee1c37903f3e7f09c2c332ff
Javi <79421409+javinoto@users.noreply.github.com>
2022-08-11 20:38:12 -0400

    Primer Párrafo

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,11 +3,8 @@ import streamlit as st
 st.title('TUTURIAL DE PYTHON CON STREAMLIB')
 st.text('Hello MOJIX')
 
-st.header('This is a header')
-st.subheader('This is a subheader')
+//st.header('This is a header')
+//st.subheader('This is a subheader')
 
-st.markdown('This is a normal Markdown')
-st.markdown('# This is a bold Markdown')
-st.markdown('## This is a thin-bold Markdown')
-st.markdown('* This is a Markdown with point')
-st.markdown('** This is a small bold Markdown **')
+st.markdown('Vamos a ver lo básico pero poderoso de Python')
+
